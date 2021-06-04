06f0dfe9010d983fe3cc46b2e5e5008a59837f5e
Olivier3231 <maffrolivier74@gmail.com>
2021-06-04 15:51:25 +0200

    add_controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Entity\Expertise;
 use App\Form\ContactType;
+use App\Repository\ExpertiseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,6 +14,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function getAllExpertises(ExpertiseRepository $expertiseRepository): Response
+    {
+        $expertises = new Expertise();
+        return $this->render('default/index.html.twig', [
+            'expertises' => $expertiseRepository->findAll(),
+        ]);
+    }
+
     /**
      * @Route("/", name="home")
      */
@@ -33,4 +47,15 @@ class DefaultController extends AbstractController
             'form' => $contactForm->createView(),
         ]);
     }
+    
+    /**
+     * @Route("/", name="home")
+     */
+    /*public function showExpertise(ExpertiseRepository $expertiseRepository): Response
+    {
+
+        return $this->render('default/index.html.twig', [
+            'expertise' => $expertiseRepository ->findOneBy(['expertiseList' => ''])
+        ]);
+    }*/
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,63 +13,52 @@ use Doctrine\Persistence\ObjectManager;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
-
     {
         $faker = \Faker\Factory::create();
-
-        $about = [$faker->name];
-        $ab = new About();
-        $ab->setTitle($faker->name())
+        $abo = new About();
+        $abo->setTitle($faker->name())
         ->setSubtitle($faker->words(4, true))
-            ->setDescription($faker->paragraph(1,true))
-            ->setAvatar('https://fr.freepik.com/photos-gratuite/avocat-client_3357722.htm#page=1&query=avocat&position=29');
-            $manager->persist($ab);
+            ->setDescription($faker->paragraph(1, true))
+            ->setAvatar
+            ('https://fr.freepik.com/photos-gratuite/avocat-client_3357722.htm#page=1&query=avocat&position=29');
+            $manager->persist($abo);
 
 
         $newscategories = [$faker->word, $faker->word];
         $arrayNewsCategories = [];
-        foreach($newscategories as $newscategory)
-        {
+        foreach ($newscategories as $newscategory) {
             $newscat = new NewsCategory();
             $newscat->setName($newscategory);
-
             $manager->persist($newscat);
-
             array_push($arrayNewsCategories, $newscat);
         }
 
-        for ($i=0; $i<4; $i++) {
+        for ($i = 0; $i < 4; $i++) {
             $new = new News();
             $new->setTitle($faker->words(3, true))
             ->setSubtitle($faker->words(5, true))
-            ->setDescription($faker->paragraph(2,true))
+            ->setDescription($faker->paragraph(2, true))
             ->setDate($faker->dateTime())
             ->setNewsCategory($faker->randomElement($arrayNewsCategories));
-
             $manager->persist($new);
         }
-        
+
         $expertiseLists = [$faker->sentence(), $faker->sentence()];
         $arrayExpertiseLists = [];
-        foreach($expertiseLists as $expertiseList)
-        {
+        foreach ($expertiseLists as $expertiseList) {
             $expList = new ExpertiseList();
             $expList->setText($expertiseList);
-
             $manager->persist($expList);
-
             array_push($arrayExpertiseLists, $expList);
         }
 
-        for ($i=0; $i<3; $i++) {
+        for ($i = 0; $i < 3; $i++) {
             $expertise = new Expertise();
             $expertise->setTitle($faker->words(3, true))
-            ->setDescription($faker->paragraph(2,true))
+            ->setDescription($faker->paragraph(2, true))
             ->setExpertiseList($faker->randomElement($arrayExpertiseLists));
-
             $manager->persist($expertise);
         }
-
         $manager->flush();
     }
 }
