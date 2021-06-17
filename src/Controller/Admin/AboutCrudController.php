c762407db8d201d9c21926b1d91503eb0e110d6c
fourneau <68759271+fourneau@users.noreply.github.com>
2021-06-17 12:05:54 +0200

    Update AboutCrudController.php

--- src/Controller/Admin/AboutCrudController.php
+++ src/Controller/Admin/AboutCrudController.php
@@ -12,14 +12,4 @@ class AboutCrudController extends AbstractCrudController
         return About::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
 }
