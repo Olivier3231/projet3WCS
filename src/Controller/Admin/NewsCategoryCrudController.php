79c74636b65fa23449370335f3afd0eab4c29552
fourneau <68759271+fourneau@users.noreply.github.com>
2021-06-17 12:10:16 +0200

    Update NewsCategoryCrudController.php

--- src/Controller/Admin/NewsCategoryCrudController.php
+++ src/Controller/Admin/NewsCategoryCrudController.php
@@ -12,14 +12,4 @@ class NewsCategoryCrudController extends AbstractCrudController
         return NewsCategory::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
 }
