4f2031bafe4b3625cfbe24b44fe4c53d8ac745f6
Olivier3231 <maffrolivier74@gmail.com>
2021-07-02 15:21:16 +0200

    add_js_animation

--- assets/app.js
+++ assets/app.js
@@ -12,11 +12,12 @@ import './styles/app.css';
 import './bootstrap';
 
 
-//texte présentation
-//$("#test").show("slide", { direction: "right" }, 2000);
+
 new Filter(document.querySelector('.js-filter'))
 var myCarousel = document.querySelector('#myCarousel')
 var carousel = new bootstrap.Carousel(myCarousel, {
     interval: 2000,
     wrap: false
 })
+
+//texte présentation
