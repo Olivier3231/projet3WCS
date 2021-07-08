0eb703bd7329d2da8d2e177d1024c84b598d755d
fourneau <cathyjulien14@orange.fr>
2021-07-08 12:29:39 +0200

    vich

---
+++ .history/src/Entity/UploadBackground_20210708115436.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UploadBackgroundRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ORM\Entity(repositoryClass=UploadBackgroundRepository::class)
+ * @Vich\Uploadable
+ */
+class UploadBackground
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $upload;
+
+    /**
+     * @
+     */
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUpload(): ?string
+    {
+        return $this->upload;
+    }
+
+    public function setUpload(string $upload): self
+    {
+        $this->upload = $upload;
+
+        return $this;
+    }
+}
---
+++ .history/src/Entity/UploadCarrousel_20210708115128.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UploadCarrouselRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+/**
+ * @ORM\Entity(repositoryClass=UploadCarrouselRepository::class)
+ * 
+ */
+class UploadCarrousel
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $upload;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUpload(): ?string
+    {
+        return $this->upload;
+    }
+
+    public function setUpload(string $upload): self
+    {
+        $this->upload = $upload;
+
+        return $this;
+    }
+}
