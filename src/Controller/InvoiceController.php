1615b4645953372314f15e8855fd83b04a24bc54
Olivier3231 <maffrolivier74@gmail.com>
2021-07-20 11:23:05 +0200

    fix_multiples_bugs

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Customer; 
-use App\Entity\Folder; 
+use App\Entity\Customer;
+use App\Entity\Folder;
 use App\Repository\AboutRepository;
 use App\Repository\CustomerRepository;
 use App\Repository\FolderRepository;
@@ -19,31 +19,64 @@ use Symfony\Component\Routing\Annotation\Route;
 use Knp\Bundle\SnappyBundle\KnpSnappyBundle;
 use Knp\Snappy\Pdf;
 use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class InvoiceController extends AbstractController
 {
+    private $aboutRepository;
+    private $customerRepository;
+    private $folderRepository;
+    private $ownerRepository;
+    private $paymentTermsRepository;
+    private $presetDiligenceRepository;
+    private $rateRepository;
+
+    public function __construct(
+        AboutRepository $aboutRepository,
+        CustomerRepository $customerRepository,
+        FolderRepository $folderRepository,
+        OwnerRepository $ownerRepository,
+        PaymentTermsRepository $paymentTermsRepository,
+        PresetDiligenceRepository $presetDiligenceRepository,
+        RateRepository $rateRepository
+    ) {
+        $this->aboutRepository = $aboutRepository;
+        $this->customerRepository = $customerRepository;
+        $this->folderRepository = $folderRepository;
+        $this->ownerRepository = $ownerRepository;
+        $this->paymentTermsRepository = $paymentTermsRepository;
+        $this->presetDiligenceRepository = $presetDiligenceRepository;
+        $this->rateRepository = $rateRepository;
+    }
+
     /**
-     * @Route("/facturation/contentieux", name="invoice")
+     * @Route("/facturation/{folderId}", name="invoice")
      */
-    public function index(AboutRepository $aboutRepository,CustomerRepository $customerRepository,FolderRepository $folderRepository,OwnerRepository $ownerRepository, PaymentTermsRepository $paymentTermsRepository, PresetDiligenceRepository $presetDiligenceRepository, RateRepository $rateRepository): Response
+    public function index(int $folderId): Response
     {
-        
-        return $this->render('invoice/indexInvoice.html.twig', [
-            'abouts' => $aboutRepository->findAll(),
-            'customers' => $customerRepository->findAll(),
-            'folders' => $folderRepository->findAll(),
-            'owners' => $ownerRepository->findAll(),
-            'paymentterms' => $paymentTermsRepository->findAll(),
-            'presetdiligences' => $presetDiligenceRepository->findBy([], ['id' => 'ASC']),
-            'rates'=>$rateRepository->findAll(),
-            'controller_name' => 'InvoiceController',]);
-    }
+        $folder = $this->folderRepository->find($folderId);
+        //dd($folder);
 
-    public function selectCustomer(EntityManagerInterface $em)
-    {
-
+        if ($folder->getBusinessType()->getName() == 'contentieux') {
+            return $this->render('invoice/indexInvoice.html.twig', [
+            'abouts' => $this->aboutRepository->findAll(),
+            'customers' => $this->customerRepository->findAll(),
+            'folders' => $this->folderRepository->findAll(),
+            'owners' => $this->ownerRepository->findAll(),
+            'paymentterms' => $this->paymentTermsRepository->findAll(),
+            'presetdiligences' => $this->presetDiligenceRepository->findBy([], ['id' => 'ASC']),
+            'rates' => $this->rateRepository->findAll(),
+            ]);
+        } else {
+            return $this->render('invoice/socialCouncil.html.twig', [
+            'abouts' => $this->aboutRepository->findAll(),
+            'customers' => $this->customerRepository->findAll(),
+            'folders' => $this->folderRepository->findAll(),
+            'owners' => $this->ownerRepository->findAll(),
+            'paymentterms' => $this->paymentTermsRepository->findAll(),
+            'presetdiligences' => $this->presetDiligenceRepository->findBy([], ['id' => 'ASC']),
+            'rates' => $this->rateRepository->findAll(),
+            ]);
+        }
     }
-    
 }
-
